1eca57024eb93c21978b64b99e5837fd36f64cfc
Ase <tdruesne05@gmail.com>
2026-02-03 16:29:22 +0100

    1000x better player

--- Assets/00_Scripts/Runtime/Player/PlayerController.cs
+++ Assets/00_Scripts/Runtime/Player/PlayerController.cs
@@ -1,328 +1,265 @@
-using System;
-using BillSimulation;
-using Unity.Cinemachine;
-using Unity.Entities;
-using Unity.Mathematics;
+using MortierFu.Shared;
 using UnityEngine;
-using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
-    /* PlayerController applies the supposed actions linked to the inputs on the player Prefab */
-    public class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviour
+{
+    #region Fields
+
+    [Header("Move Settings")]
+    public float walkSpeed = 30f;
+    public float sprintSpeed = 70f;
+
+    [SerializeField] private float sensitivity = 1f;
+    [SerializeField] private float upDownLookRange = 80f;
+
+    [Header("Jump Settings")]
+    [SerializeField] private float jumpForce = 10;
+    [SerializeField] private float airMultiplier = 0.4f;
+    [SerializeField] private float groundDrag, airDrag;
+    [SerializeField] private float maxAddedGravity, speedAddedGravity;
+    [SerializeField] private float coyoteTime = 0.2f;
+
+    [Header("References")]
+    [SerializeField] private PlayerInput playerInput;
+    [SerializeField] private Rigidbody playerRigidbody;
+    [SerializeField] private Transform feet;
+    [SerializeField] private Transform headTransform;
+    [SerializeField] private Camera playerCam;
+
+    private Vector2 _rotVector;
+    public Vector2 MoveVector { get; private set; }
+    
+    private float _verticalRotation;
+
+    public float CurrentSpeed { get; private set; }
+
+    public bool isGrounded;
+    private float _currentAddedGravity;
+    private float _currentFallTime;
+
+    private RaycastHit _slopeHit;
+    private Vector3 _moveDir;
+    private Vector3 _slopeMoveDir;
+
+    private Transform _parentTransform;
+
+    #endregion
+
+    private void Awake()
     {
-
-
-        #region Fields
-        [Header("Move Settings")]
-        [Tooltip("Base movement speed when walking.")]
-        public float _walkSpeed = 30f;
-        public float _sprintSpeed = 70f;
-        
-        [Tooltip("Sensitivity of the mouse when looking around.")]
-
-        [SerializeField] private float _sensitivity = 1f;
-        
-        [Tooltip("Height size of the Character")]
-        [SerializeField] private float _characterSize = 2f;
-
-        [Tooltip("Maximum vertical angle the player can look up or down.")]
-        [Range(0, 90)]
-        [SerializeField] private float _upDownLookRange = 80f;
-        
-        [Header("Jump Settings")]
-        [SerializeField] private float jumpForce = 10;
-        [SerializeField] private float airMultiplier = 0.4f;
-        [SerializeField] private float groundDrag, airDrag;
-        [SerializeField] private float maxAddedGravity, speedAddedGravity;
-        private float currentAddedGravity;
-        private RaycastHit slopeHit;
-        [SerializeField] private float coyoteTime = 0.2f;
-        private float currentFallTime;
-        
-        [Header("Camera effects")]
-        [SerializeField] private Camera playerCam;
-        [SerializeField] private float defaultFOV = 100;
-
-        [Header("Interaction Settings")]
-        [SerializeField] float interactionRange = 10;
-        [SerializeField] private float interactionRadius = 0.25f; 
-        [SerializeField] private LayerMask interactableLayer;
-        
-        [Header("References")]
-        [SerializeField] private Transform feet;
-        [SerializeField] private Transform headTransform;
-        [SerializeField] PlayerInput _playerControls;
-        [SerializeField] Rigidbody _playerRigidbody;
-        
-        private InputAction _movementAction;
-        private InputAction _rotationAction;
-        private InputAction _jumpAction;
-        private InputAction _interactAction;
-        private InputAction _sprintAction;
-
-        private Vector3 moveDir, slopeMoveDir;
-        public Vector2 moveVector, rotVector;
-        private Transform parentTransform;
-        private float _verticalRotation;
-        public float _currentSpeed;
-        private bool isMoving;
-        private bool isGrounded;
-
-        #endregion
-        
-        #region Entity Fields
-        
-        private Entity playerEntity;
-        private EntityManager entityManager;
-        
-        #endregion
-
-        #region Methods
-        void Start()
-        {
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;
-            
-            parentTransform = transform.root.transform;
-            
-            SetInputReferences();
-            
-            playerCam.fieldOfView = defaultFOV;
-            
-            entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
-            _currentSpeed = _walkSpeed;
-
-            /*playerEntity = entityManager.CreateEntity(
-                typeof(PlayerTag),
-                typeof(PlayerMovement)
-            );*/
-        }
-        
-        void Update()
-        {
-            GroundCheck();
-            
-            ControlDrag();
-            
-            if (isGrounded && _playerRigidbody.linearVelocity.y < 0)
-            {
-                currentFallTime = 0;
-            }
-
-            /*if (entityManager.Exists(playerEntity))
-            {
-                float3 velocity = _playerRigidbody.linearVelocity / Time.deltaTime;
-
-                entityManager.SetComponentData(playerEntity, new PlayerMovement
-                {
-                    Position = transform.position,
-                    Velocity = velocity,
-                    CollisionRadius = 0.5f
-                });
-            }*/
-        }
-
-        private void FixedUpdate()
-        {
-            HandleMovement();
-            HandleRotation();
-            
-            ResetSprint();
-            
-            if (!isGrounded)
-            {
-                currentAddedGravity = Mathf.SmoothStep(currentAddedGravity, maxAddedGravity, speedAddedGravity * Time.deltaTime);
-
-                if (currentFallTime < coyoteTime + 0.1f)
-                {
-                    currentFallTime += Time.deltaTime;
-                }
-            }
-            else if (currentAddedGravity != 0)
-            {
-                currentAddedGravity = 0;
-            }
-        }
-
-        private void GroundCheck()
-        {
-            Color color = Color.red;
-            
-            isGrounded = Physics.Raycast(feet.position+ new Vector3(0,0.1f,0), Vector3.down, 0.5f);
-            
-            if(isGrounded) color = Color.green;
-            
-            Debug.DrawRay(feet.position, Vector3.down * 0.5f, color);
-        }
-
-        private void OnTriggerEnter(Collider other)
-        {
-            if (other.gameObject.layer == LayerMask.NameToLayer("TriggerZone"))
-            {
-                if (parentTransform.parent  != other.transform)
-                {
-                    parentTransform.parent = other.transform;
-                }
-            }
-        }
-
-        private void OnTriggerExit(Collider other)
-        {
-            if (other.gameObject.layer == LayerMask.NameToLayer("TriggerZone"))
-            {
-                if (parentTransform.parent  == other.transform)
-                {
-                    parentTransform.parent  = null;
-                }
-            }
-        }
+        _parentTransform = transform.root;
+        CurrentSpeed = walkSpeed;
         
         
-        void Jump()
-        {
-            if (currentFallTime < coyoteTime && _playerRigidbody)
-            {
-                _playerRigidbody.linearVelocity = new Vector3(_playerRigidbody.linearVelocity.x, 0, _playerRigidbody.linearVelocity.z);
-                _playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-
-                currentFallTime = coyoteTime + 1;
-            }
-        }
-
-        void Sprint()
-        {
-            if (_currentSpeed == _walkSpeed) _currentSpeed = _sprintSpeed;
-            else _currentSpeed = _walkSpeed;
-        }
-
-        void ResetSprint()
-        {
-            if (_playerRigidbody.linearVelocity.magnitude < 1 && _currentSpeed == _sprintSpeed)
-            {
-                _currentSpeed = _walkSpeed;
-            }
-        }
-        
-        private void TryInteract()
-        {
-            RaycastHit hit; 
-            
-            if (headTransform && Physics.SphereCast(headTransform.position, interactionRadius, headTransform.forward, out hit, interactionRange, interactableLayer))
-            {
-                Debug.Log("Interacting with " + hit.collider.name);
-                if (hit.collider.gameObject.TryGetComponent(out IInteractable interactable))
-                {
-                    interactable.Interact(transform);
-                }
-            }
-        }
-
-
-        private void HandleMovement()
-        {
-            
-            slopeMoveDir = Vector3.ProjectOnPlane(moveDir, slopeHit.normal);
-
-            Vector3 forward = playerCam.transform.forward;
-            forward.y = 0;
-        
-            Vector3 right = playerCam.transform.right;
-            forward.y = 0;
-            
-            moveDir = forward.normalized * moveVector.y + right * moveVector.x;
-
-            _playerRigidbody.AddForce(Vector3.down * currentAddedGravity);
-        
-            if (isGrounded && !OnSlope())
-            {
-                _playerRigidbody.AddForce(moveDir.normalized * _currentSpeed, ForceMode.Acceleration);
-            }
-            else if (isGrounded && OnSlope())
-            {
-                _playerRigidbody.AddForce(slopeMoveDir.normalized * _currentSpeed, ForceMode.Acceleration);
-            }
-            else if (!isGrounded)
-            {
-                _playerRigidbody.AddForce(moveDir.normalized * _currentSpeed * airMultiplier, ForceMode.Acceleration);
-            }
-        }
-        
-        void ControlDrag()
-        {
-            if (OnSlope() && moveDir.magnitude <= 0.1f)
-            {
-                _playerRigidbody.linearDamping = 30;
-            }
-            else if (isGrounded)
-            {
-                _playerRigidbody.linearDamping = groundDrag;
-            }
-            else
-            {
-                _playerRigidbody.linearDamping = airDrag;
-            }
-        }
-        
-        private bool OnSlope()
-        {
-            if (Physics.Raycast(feet.position, Vector3.down, out slopeHit, 0.2f))
-            {
-                if (slopeHit.normal != Vector3.up)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-            return false;
-        }
-        
-        private void HandleRotation()
-        {
-            float mouseXRotation = rotVector.x * _sensitivity;
-            float mouseYRotation = rotVector.y * _sensitivity;
-
-            transform.Rotate(0, mouseXRotation, 0);
-            
-            _verticalRotation = Mathf.Clamp(_verticalRotation - mouseYRotation, -_upDownLookRange, _upDownLookRange);
-            headTransform.localRotation = Quaternion.Euler(_verticalRotation, 0, 0);
-
-        }
-        
-        public void SetInputReferences()
-        {
-            if (_playerControls == null)
-                throw new MissingReferenceException("Please ensure that the player controls are assigned in the player input handler :)");
-            
-            _movementAction = _playerControls.actions["Movement"];
-            _rotationAction = _playerControls.actions["Rotation"];
-            _jumpAction = _playerControls.actions["Jump"];
-            _interactAction = _playerControls.actions["Interaction"];
-            _sprintAction =  _playerControls.actions["Sprint"];
-
-            SubscribeActionValuesToInputEvents();
-        }
-        
-        private void SubscribeActionValuesToInputEvents()
-        {
-            _movementAction.performed += inputInfo => moveVector = inputInfo.ReadValue<Vector2>();
-            _movementAction.canceled += inputInfo => moveVector  = Vector2.zero;
-
-            _rotationAction.performed += inputInfo => rotVector = inputInfo.ReadValue<Vector2>();
-            _rotationAction.canceled += inputInfo => rotVector = Vector2.zero;
-            
-            _sprintAction.started += inputInfo => Sprint();
-            
-            
-            _jumpAction.started += inputInfo => Jump();
-
-            _interactAction.started += inputInfo => TryInteract();
-
-        }
-
-        
-        #endregion
     }
 
+    private void OnEnable()
+    {
+        playerInput.ActivateInput();
+        SwitchActionMap("Player");
+    }
+
+    private void OnDisable()
+    {
+        playerInput.DeactivateInput();
+    }
+
+    private void Update()
+    {
+        GroundCheck();
+        ControlDrag();
+
+        if (isGrounded && playerRigidbody.linearVelocity.y < 0)
+            _currentFallTime = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        HandleMovement();
+        HandleRotation();
+        ApplyExtraGravity();
+        ResetSprintIfNotMoving();
+    }
+
+    #region INPUT CALLBACKS
+
+    public void OnMovement(InputValue inputValue)
+    {
+        Logs.LogError("OnMovement called");
+        MoveVector = inputValue.Get<Vector2>();
+    }
+
+    public void OnRotation(InputValue inputValue)
+    {
+        _rotVector = inputValue.Get<Vector2>();
+    }
+
+    public void OnJump(InputValue inputValue)
+    {
+        if (inputValue.isPressed)
+            Jump();
+    }
+
+    public void OnSprint(InputValue inputValue)
+    {
+        // C'est un hold parce que c'est mieux
+        CurrentSpeed = inputValue.isPressed ? sprintSpeed : walkSpeed;
+    }
+
+    public void OnInteraction(InputValue inputValue)
+    {
+        if (inputValue.isPressed)
+            TryInteract();
+    }
+
+    #endregion
+
+    #region ACTION MAP
+
+    private void SwitchActionMap(string map)
+    {
+        playerInput.SwitchCurrentActionMap(map);
+    }
+
+    public void EnterVehicle()
+    {
+        SwitchActionMap("Driving");
+    }
+
+    public void ExitVehicle()
+    {
+        SwitchActionMap("Player");
+    }
+
+    #endregion
+
+    #region Movement
+
+    private void HandleMovement()
+    {
+        Vector3 forward = playerCam.transform.forward;
+        Vector3 right = playerCam.transform.right;
+
+        forward.y = 0;
+        right.y = 0;
+
+        _moveDir = forward.normalized * MoveVector.y +
+                  right.normalized * MoveVector.x;
+
+        _slopeMoveDir = Vector3.ProjectOnPlane(_moveDir, _slopeHit.normal);
+
+        playerRigidbody.AddForce(Vector3.down * _currentAddedGravity);
+
+        if (isGrounded)
+        {
+            Vector3 dir = OnSlope() ? _slopeMoveDir : _moveDir;
+            playerRigidbody.AddForce(dir.normalized * CurrentSpeed, ForceMode.Acceleration);
+        }
+        else
+        {
+            playerRigidbody.AddForce(_moveDir.normalized * (CurrentSpeed * airMultiplier),
+                ForceMode.Acceleration);
+        }
+    }
+
+    private void HandleRotation()
+    {
+        float mouseX = _rotVector.x * sensitivity;
+        float mouseY = _rotVector.y * sensitivity;
+
+        transform.Rotate(0, mouseX, 0);
+
+        _verticalRotation = Mathf.Clamp(
+            _verticalRotation - mouseY,
+            -upDownLookRange,
+            upDownLookRange);
+
+        headTransform.localRotation = Quaternion.Euler(_verticalRotation, 0, 0);
+    }
+
+    #endregion
+
+    #region Physics
+
+    private void GroundCheck()
+    {
+        isGrounded = Physics.Raycast(feet.position + Vector3.up * 0.1f, Vector3.down, 0.5f);
+    }
+
+    private bool OnSlope()
+    {
+        if (Physics.Raycast(feet.position, Vector3.down, out _slopeHit, 0.2f))
+            return _slopeHit.normal != Vector3.up;
+
+        return false;
+    }
+
+    private void ControlDrag()
+    {
+        if (OnSlope() && _moveDir.magnitude <= 0.1f)
+            playerRigidbody.linearDamping = 30;
+        else if (isGrounded)
+            playerRigidbody.linearDamping = groundDrag;
+        else
+            playerRigidbody.linearDamping = airDrag;
+    }
+
+    private void ApplyExtraGravity()
+    {
+        if (!isGrounded)
+        {
+            _currentAddedGravity = Mathf.SmoothStep(
+                _currentAddedGravity,
+                maxAddedGravity,
+                speedAddedGravity * Time.deltaTime);
+
+            if (_currentFallTime < coyoteTime + 0.1f)
+                _currentFallTime += Time.deltaTime;
+        }
+        else
+        {
+            _currentAddedGravity = 0;
+        }
+    }
+
+    #endregion
+
+    #region Actions
+
+    private void Jump()
+    {
+        if (_currentFallTime >= coyoteTime) return;
+
+        playerRigidbody.linearVelocity =
+            new Vector3(playerRigidbody.linearVelocity.x, 0, playerRigidbody.linearVelocity.z);
+
+        playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
+        _currentFallTime = coyoteTime + 1;
+    }
+
+    private void ResetSprintIfNotMoving()
+    {
+        if (playerRigidbody.linearVelocity.magnitude < 1f)
+            CurrentSpeed = walkSpeed;
+    }
+
+    private void TryInteract()
+    {
+        if (!headTransform) return;
+
+        if (Physics.SphereCast(
+                headTransform.position,
+                0.25f,
+                headTransform.forward,
+                out var hit,
+                10f))
+        {
+            if (hit.collider.TryGetComponent(out IInteractable interactable))
+                interactable.Interact(transform);
+        }
+    }
+
+    #endregion
+}
