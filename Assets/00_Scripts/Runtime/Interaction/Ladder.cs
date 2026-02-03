039a35c36e3b0f575054a601f0bca65bb2d2b8c7
pharreller <phafoubastien@gmail.com>
2026-02-03 14:20:23 +0100

    Ladder script

--- Assets/00_Scripts/Runtime/Interaction/Ladder.cs
+++ Assets/00_Scripts/Runtime/Interaction/Ladder.cs
@@ -2,7 +2,11 @@ using UnityEngine;
 
 public class Ladder : MonoBehaviour, IInteractable
 {
+    [SerializeField] private float power = 50;
     public void Interact(Transform interactor)
     {
+        interactor.TryGetComponent(out Rigidbody rb);
+        
+        rb?.AddForce(transform.up * power, ForceMode.Impulse);
     }
 }
