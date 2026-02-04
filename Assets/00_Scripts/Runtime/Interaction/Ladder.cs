4a53593576896b7b22ce6e049df842edf8ec5086
pharreller <phafoubastien@gmail.com>
2026-02-04 12:20:48 +0100

    ladder ajustement

--- Assets/00_Scripts/Runtime/Interaction/Ladder.cs
+++ Assets/00_Scripts/Runtime/Interaction/Ladder.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 public class Ladder : MonoBehaviour, IInteractable
 {
-    [SerializeField] private float power = 50;
+    [SerializeField] private float power = 1000;
     public void Interact(Transform interactor)
     {
         interactor.TryGetComponent(out Rigidbody rb);
         
-        rb?.AddForce(transform.up * power, ForceMode.Impulse);
+        rb?.AddForce(transform.up * power, ForceMode.Acceleration);
     }
 }
